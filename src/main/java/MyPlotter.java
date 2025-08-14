26fcbbe49159d862c7c842c46e2118ad0689f443
Tobias Grothe <mail@tobias-1.de>
2025-08-14 23:19:03 +0200

    Update

--- src/main/java/MyPlotter.java
+++ src/main/java/MyPlotter.java
@@ -2,7 +2,6 @@ import java.awt.image.RenderedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.lang.reflect.Method;
 import java.util.Base64;
 import javax.imageio.ImageIO;
 import org.knowm.xchart.BitmapEncoder;
@@ -10,34 +9,6 @@ import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYChartBuilder;
 
 public class MyPlotter {
-  @Deprecated
-  public static String plot(Object obj) throws Exception {
-    Method numberOfSeries = obj.getClass().getMethod("numberOfSeries");
-    Method generateSeries = obj.getClass().getMethod("generateSeries", int.class);
-    Method getTitle = obj.getClass().getMethod("getTitle", int.class);
-    final int numberOfSeriesValue = (Integer) numberOfSeries.invoke(obj);
-
-    XYChart chart =
-        new XYChartBuilder()
-            .width(800)
-            .height(400)
-            .title("Area Chart")
-            .xAxisTitle("x")
-            .yAxisTitle("y")
-            .build();
-    for (int i = 0; i < numberOfSeriesValue; i++) {
-      double[][] series = (double[][]) generateSeries.invoke(obj, i);
-      double[] xData = new double[series.length];
-      double[] yData = new double[series.length];
-      for (int j = 0; j < series.length; j++) {
-        xData[j] = series[j][0];
-        yData[j] = series[j][1];
-      }
-      chart.addSeries((String) getTitle.invoke(obj, i), xData, yData);
-    }
-    return imgToBase64String(BitmapEncoder.getBufferedImage(chart));
-  }
-
   @Deprecated
   public static String plot(MySupplier supplier) {
     XYChart chart =
@@ -61,7 +32,7 @@ public class MyPlotter {
     return imgToBase64String(BitmapEncoder.getBufferedImage(chart));
   }
 
-  private static String imgToBase64String(RenderedImage img) {
+  public static String imgToBase64String(RenderedImage img) {
     ByteArrayOutputStream os = new ByteArrayOutputStream();
     try {
       ImageIO.write(img, "PNG", os);
