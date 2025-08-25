3091354063d2c8cf397dec693b59ffa9fa81bd62
Tobias Grothe <mail@tobias-1.de>
2025-08-25 17:36:31 +0200

    Update (#21)

--- src/main/java/MyHttpServer.java
+++ src/main/java/MyHttpServer.java
@@ -4,35 +4,46 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.UncheckedIOException;
 import java.net.InetSocketAddress;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.StringTokenizer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import org.apache.commons.lang3.exception.UncheckedException;
 import org.apache.commons.text.StringSubstitutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MyHttpServer {
+  public record MyPreHttpHandler(Function<HttpHandler, Boolean> preHandler) {}
+
   public record MyHttpHandler(String path, HttpHandler handler) {}
 
   public enum ContentType {
     HTML("text/html"),
     JSON("application/json"),
     TEXT("text/plain");
+    private final String type;
 
-    ContentType(String type) {}
+    ContentType(String type) {
+      this.type = type;
+    }
+
+    @Override
+    public String toString() {
+      return type;
+    }
   }
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MyHttpServer.class);
-
+  private static final Object LOCK = new Object();
   private static final HashMap<String, String> templates = new HashMap<>();
 
-  public static void start(MyHttpHandler... handlers) throws IOException {
+  public static void start(MyPreHttpHandler preHandler, MyHttpHandler... handlers)
+      throws IOException {
     if (handlers == null || handlers.length == 0) {
       throw new IllegalArgumentException("At least one handler must be provided.");
     }
@@ -51,16 +62,28 @@ public class MyHttpServer {
                 getClientIpAddress(exchange),
                 exchange.getRequestURI());
             try {
-              handler.handler().handle(exchange);
+              synchronized (LOCK) {
+                if (preHandler != null) {
+                  if (preHandler.preHandler().apply(handler.handler())) {
+                    handler.handler().handle(exchange);
+                  }
+                } else {
+                  handler.handler().handle(exchange);
+                }
+              }
+              if (exchange.getResponseCode() == -1) {
+                LOGGER.warn("No response sent for path: {}", handler.path());
+                sendCustomResponse(
+                    exchange, 404, ContentType.TEXT, null, "Not found or access denied.");
+              }
             } catch (Exception e) {
-              LOGGER.warn("Exception in handler for path {}: {}", handler.path(), e.getMessage());
+              LOGGER.warn("Exception in handler for path {}", handler.path(), e);
               sendCustomResponse(
                   exchange,
-                  500,
+                  404,
                   ContentType.TEXT,
                   null,
-                  "An error occurred while processing your request.",
-                  e.getMessage());
+                  "An error occurred while processing your request.");
             }
             LOGGER.info(
                 "Request processing completed for client: {} with status code: {}",
@@ -88,12 +111,15 @@ public class MyHttpServer {
   public static String getPathPart(HttpExchange ex, int index) {
     String[] parts = ex.getRequestURI().getRawPath().split("/");
     if (index < 0 || index >= parts.length) {
-      return null;
+      throw new IllegalArgumentException("Index out of bounds for path parts.");
     }
     return URLDecoder.decode(parts[index], StandardCharsets.UTF_8);
   }
 
   private static String getTemplate(String path, String... args) throws IOException {
+    if (path == null || path.isBlank() || args == null || args.length == 0) {
+      throw new IllegalArgumentException("Path and args cannot be null or empty.");
+    }
     if (!templates.containsKey(path)) {
       try (InputStream templateStream = Main.class.getResourceAsStream(path)) {
         assert templateStream != null;
@@ -121,17 +147,17 @@ public class MyHttpServer {
       String... templateValues) {
     try {
       String responseBody =
-          templatePath != null
+          contentType == ContentType.HTML && templatePath != null
               ? getTemplate(templatePath, templateValues)
-              : Arrays.toString(templateValues);
+              : String.join("\n", templateValues);
+      byte[] bytes = responseBody.getBytes(StandardCharsets.UTF_8);
       exchange.getResponseHeaders().add("Content-Type", contentType + "; charset=utf-8");
-      exchange.sendResponseHeaders(statusCode, responseBody.length());
+      exchange.sendResponseHeaders(statusCode, bytes.length);
       try (OutputStream os = exchange.getResponseBody()) {
-        os.write(responseBody.getBytes(StandardCharsets.UTF_8));
+        os.write(bytes);
       }
-    } catch (IOException e) {
-      LOGGER.error("Failed to send custom response: {}", e.getMessage());
-      throw new UncheckedIOException(e);
+    } catch (Exception e) {
+      throw new UncheckedException(e);
     }
   }
 }
